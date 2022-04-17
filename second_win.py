82afe666ddb33faff21b0b0687520210e4ac3a0c
vinishko <tobeseller@gmail.com>
2022-04-17 22:16:31 +0300

    ня

--- second_win.py
+++ second_win.py
@@ -1,1 +1,34 @@
-# напиши здесь код для второго экрана приложения
+from final_win import*
+from instr import*
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QVBoxLayout,
+    QHBoxLayout,
+    QMessageBox,
+    QLineEdit,
+    QPushButton
+)
+from PyQt5.QtCore import Qt
+class TestWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        pass
+    def connects(self):
+        pass
+    def next_click(self):
+        pass
+app = QApplication([])
+wn = TestWin()
+app.exec_()
