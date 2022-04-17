b74f639843b16f9b2b26eb9fe420288e7d98449b
Katrin <a258307@yandex.ru>
2022-04-17 21:25:51 +0300

    Новые инструкции

--- final_win.py
+++ final_win.py
@@ -1,1 +1,21 @@
 # напиши здесь код третьего экрана приложения
+from instr import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QVBoxLayout,
+    QHBoxLayout,
+    QMessageBox,
+    QLineEdit,
+    QPushButton
+)
+
+class FinalWin(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.set_appear()
+    self.initUI()
+    self.connects()
+    self.show()
--- my_app.py
+++ my_app.py
@@ -1,1 +1,25 @@
 # напиши здесь код основного приложения и первого экрана
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QVBoxLayout,
+    QHBoxLayout,
+    QMessageBox,
+    QLineEdit,
+    QPushButton
+)
+
+class MainWin(QWidget):
+  def __init__(self):
+    super().__init__()
+  def set_appear(self): 
+    self.setWindowTitle(txt_title)
+    self.resize(win_width, win_height)
+    self.move(win_x, win_y)
+
+
+    self.initUI()
+    self.connects()
+    self.show()
