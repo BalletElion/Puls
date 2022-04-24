e00ea941a4bf28c4d0c8b83b32605dfb5dc96865
Katrin <a258307@yandex.ru>
2022-04-24 22:56:51 +0300

    The END

--- final_win.py
+++ final_win.py
@@ -12,10 +12,121 @@ from PyQt5.QtWidgets import (
     QPushButton
 )
 
+
 class FinalWin(QWidget):
-  def __init__(self):
-    super().__init__()
-    self.set_appear()
-    self.initUI()
-    self.connects()
-    self.show()
+    def __init__(self, age, P1, P2, P3):
+        super().__init__()
+        self.age = age
+        self.P1 = P1
+        self.P2 = P2
+        self.P3 = P3
+        self.set_appear()
+        self.initUI()
+        #self.connects()
+        self.show()
+    def set_appear(self): 
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI (self):
+        self.result1 = QLabel(self.rufie (self.age, self.P1, self.P2, self.P3))
+        self.index = QLabel(str(self.result))
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.index, alignment = Qt.AlignCenter)
+        self.layout.addWidget(self.result1, alignment = Qt.AlignCenter)
+        self.setLayout(self.layout)
+
+    def rufie (self, age, P1, P2, P3):
+        self.result = (4 * (P1 + P2 + P3) - 200) / 10
+        result = self.result
+        if age == 7 or age == 8:
+            if result >= 21:
+                #низкий
+                return txt_res1
+            elif result >= 17 and result <= 20.9:
+                #удовлетворительный
+                return txt_res2
+            elif result >= 12 and result <= 16.9:
+                #средний
+                return txt_res3
+            elif result >= 6.5 and result <= 11.9:
+                #выше среднего
+                return txt_res4
+            else:
+                #высокий
+                return txt_res5
+        
+
+        elif age == 9 or age == 10:
+            if result >= 19.5:
+                #низкий
+                return txt_res1
+            elif result >= 15.5 and result <= 19.4:
+                #удовлетворительный
+                return txt_res2
+            elif result >= 10.5 and result <= 15.4:
+                #средний
+                return txt_res3
+            elif result >= 5 and result <= 10.4:
+                #выше среднего
+                return txt_res4
+            else:
+                #высокий
+                return txt_res5
+
+
+        elif age == 11 or age == 12:
+            if result >= 18:
+                #низкий
+                return txt_res1
+            elif result >= 14 and result <= 17.9:
+                #удовлетворительный
+                return txt_res2
+            elif result >= 9 and result <= 13.9:
+                #средний
+                return txt_res3
+            elif result >= 3.5 and result <= 8.9:
+                #выше среднего
+                return txt_res4
+            else:
+                #высокий
+                return txt_res5
+
+
+        elif age == 13 or age == 14:
+            if result >= 16.5:
+                #низкий
+                return txt_res1
+            elif result >= 12.5 and result <= 16.4:
+                #удовлетворительный
+                return txt_res2
+            elif result >= 7.5 and result <= 12.4:
+                #средний
+                return txt_res3
+            elif result >= 2 and result <= 7.4:
+                #выше среднего
+                return txt_res4
+            else:
+                #высокий
+                return txt_res5
+
+        elif age >= 15:
+            if result >= 15:
+                #низкий
+                return txt_res1
+            elif result >= 11 and result <= 14.9:
+                #удовлетворительный
+                return txt_res2
+            elif result >= 6 and result <= 10.9:
+                #средний
+                return txt_res3
+            elif result >= 0.5 and result <= 5.9:
+                #выше среднего
+                return txt_res4
+            else:
+                #высокий
+                return txt_res5
+
+app = QApplication([])
+wn = FinalWin(14, 50, 60, 40)
+app.exec_() 
